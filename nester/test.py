42181befcbb143c4fa565ff401931ebf737471f5
liguitong <liguitong@aliyun.com>
2018-01-20 22:09:17 +0800

    add anothe parameter to function

---
+++ nester/test.py
@@ -0,0 +1,3 @@
+import nester
+movies=["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91, ["Graham Chapman", ["Michael Palin", "John Cleese", "Terrylliam", "Eric Idle", "Terry Jones"]]]
+nester.print_lol(movies, 0)
