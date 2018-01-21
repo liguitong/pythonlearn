866d30daea918b545ebbffd21aa6281aa0ebdb1d
liguitong <liguitong@aliyun.com>
2018-01-21 11:38:01 +0800

    change the version to 1.2.0

--- nester/setup.py
+++ nester/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
       name='nester_tiler',
-      version='1.1.0',
+      version='1.2.0',
       py_modules=['nester'],
       author='tiler',
       author_email='ligutong@aliyun.com',
--- nester/test.py
+++ nester/test.py
@@ -1,4 +1,5 @@
 import nester
 names=["John", "Eric", ["Cleese", "Idle"], "Michael", ["Palin"]]
 nester.print_lol(names, 0)
-
+names.append('lgt')
+nester.print_lol(names,True,1)
