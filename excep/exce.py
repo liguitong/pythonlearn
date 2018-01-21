e851c0d0b59086ba7baeab792e6f3556eee95e8b
liguitong <liguitong@aliyun.com>
2018-01-21 14:52:23 +0800

    except the special exception

--- excep/exce.py
+++ excep/exce.py
@@ -1,12 +1,17 @@
+import os
 """ A test program to test exception"""
-data=open('/home/lgt/Program/pythonlearn/excep/sketch.txt')
-for each_line in data:
-    try:
-        if not each_line.find(':')== -1:
-            (role,line_spoken) = each_line.split(':',1)
-            print(role,end='')
-            print(' said:',end='')
-            print(line_spoken, end='')
-    except:
-        pass
-data.close()
+try:
+    data=open('/home/lgt/Program/pythonlearn/excep/sketch.txt')
+    for each_line in data:
+        try:
+            if not each_line.find(':')== -1:
+                (role,line_spoken) = each_line.split(':',1)
+                print(role,end='')
+                print(' said:',end='')
+                print(line_spoken, end='')
+        except ValueError:
+            pass
+    data.close()
+except IOError:
+    print("The datafile is missing")
+
