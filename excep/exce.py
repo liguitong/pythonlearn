0d801c8e975c29521deaa1c4bcfc69ecb666812d
liguitong <liguitong@aliyun.com>
2018-01-24 20:16:52 +0800

    use with statement to replace manage file manully

--- excep/exce.py
+++ excep/exce.py
@@ -1,17 +1,29 @@
 import os
 """ A test program to test exception"""
+man = []
+other = []
 try:
     data=open('/home/lgt/Program/pythonlearn/excep/sketch.txt')
     for each_line in data:
         try:
             if not each_line.find(':')== -1:
                 (role,line_spoken) = each_line.split(':',1)
-                print(role,end='')
-                print(' said:',end='')
-                print(line_spoken, end='')
+                line_spoken = line_spoken.strip()
+                if role == 'Man':
+                    man.append(line_spoken)
+                elif role == 'Other Man':
+                    other.append(line_spoken)
         except ValueError:
             pass
     data.close()
 except IOError:
     print("The datafile is missing")
-
+try:
+    with open('man_data.txt', 'w') as man_data,open('other_data.txt', 'w') as other_data:
+        print(man,file=man_data)
+        print(other,file=other_data)
+except IOError as err:
+    print("Can't not write to file" + str(err))
+finally:
+    man_data.close()
+    other_data.close()
