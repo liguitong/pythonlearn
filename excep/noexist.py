747950e7551e5dff25e782f6d841e932f005e791
liguitong <liguitong@aliyun.com>
2018-01-24 20:07:06 +0800

    add the noexist python file to test exception when file not exist

---
+++ excep/noexist.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+""" A demo to test  exception"""
+try:
+    data = open('missing.txt')
+    print(data.readline(),end='')
+except IOError as err:
+    print('File Error' + str(err))
+finally:
+    if 'data' in locals():
+        data.close()
