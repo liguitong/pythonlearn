1897a75ca437686093bb3e6600dc386a872d0247
liguitong <liguitong@aliyun.com>
2018-01-24 20:24:19 +0800

    add another parameter to print_lol,which specify the dest file to write

--- nester/nester.py
+++ nester/nester.py
@@ -2,14 +2,14 @@
    print_lol which to print a list that include 
    another list also"""
   
-def print_lol(the_list, indent=False, level=0):
+def print_lol(the_list, indent=False, level=0, fn=sys.stdout):
     """The function use a parameter named the_list to print,
     one item one line"""
     for each_item in the_list:
         if isinstance(each_item,list):
-            print_lol(each_item, indent, level+1)
+            print_lol(each_item, indent, level+1,fn)
         else:
             if indent:
                 for step in range(level):
-                    print("\t", end='')
-            print(each_item)
+                    print("\t", end='',file=fn)
+            print(each_item, file=fn)
